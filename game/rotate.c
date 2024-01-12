a9e5ce0c825c6f8f4a2158fc5e13cb6cb890752a
Victor Naslund Cuesta <victor.naslund@yahoo.com>
2024-01-12 16:18:26 +0100

    rotation logic fixed (y-axis inverted)

--- game/rotate.c
+++ game/rotate.c
@@ -6,12 +6,13 @@
 /*   By: vnaslund <vnaslund@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 17:11:49 by vnaslund          #+#    #+#             */
-/*   Updated: 2024/01/12 15:09:09 by vnaslund         ###   ########.fr       */
+/*   Updated: 2024/01/12 16:17:42 by vnaslund         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
+// Angle = 1 if rotating left and -1 if rotating right
 void	rotate(t_game *game, int angle)
 {
 	double	new_angle;
@@ -23,6 +24,6 @@ void	rotate(t_game *game, int angle)
 		new_angle += 2 * M_PI;
 	game->player->view_angle = new_angle;
 	game->player->x_viewdir = cos(game->player->view_angle);
-	game->player->y_viewdir = sin(game->player->view_angle);
+	game->player->y_viewdir = -sin(game->player->view_angle);
 	printf("rotation detected\n");
 }
