652d30115a2d8f5d3f1de4ad5308e5b9a88f2e03
Gabriela <krustagabriela@gmail.com>
2024-01-16 10:38:45 +0100

    wall_check

--- raycasting/raycasting_utils.c
+++ raycasting/raycasting_utils.c
@@ -6,7 +6,7 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 13:26:55 by gkrusta           #+#    #+#             */
-/*   Updated: 2024/01/15 18:54:45 by gkrusta          ###   ########.fr       */
+/*   Updated: 2024/01/16 10:37:38 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,18 @@ double	get_absoulte(double ray_angle, double view_angle)
 	return (fabs(ray_angle - view_angle));
 }
 
-bool	wall_check(char **map, t_ray *ray, t_pos first_step, char corssing)
+bool	wall_check(char **map, t_ray *ray, t_pos first_step, char crossing)
 {
 	int		i;
 	int		j;
 
 	i = (int)first_step.x;
-	j = (int)firs_step.y;
+	j = (int)first_step.y;
 	if (crossing == 'y' && ray->sign.x == -1)
 		i--;
 	if (crossing == 'x' && ray->sign.y == -1)
 		j--;
+	if (j == 0 || j == )
 	if (map[i][j] == '1')
 		return (true);
 	return (false);
